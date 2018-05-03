49aab06c921063b685436068deee62e3cd5ad036
HoangNH <hoangnhonline@gmail.com>
2018-05-03 20:12:07 +0700

    .

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -83,30 +83,28 @@ class HomeController extends Controller
         }       
         $video_url = $poster_url = '';
         if($origin_url != ''){
+           // var_dump($this->get_client_ip());
             //dd($origin_url);
-            $ch = curl_init();
-            curl_setopt( $ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows; U; Windows NT 5.1; rv:1.7.3) Gecko/20041001 Firefox/0.10.1" );
-            curl_setopt( $ch, CURLOPT_URL, $origin_url );
-            curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, true );
-            if( strpos($origin_url, 'play.to')){
-               // curl_setopt($ch, CURLOPT_REFERER, "http://fastplay.to/");                
-            }
-           curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
-            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+            if( !strpos($origin_url, 'play.to')){
+                $ch = curl_init();
+                curl_setopt( $ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows; U; Windows NT 5.1; rv:1.7.3) Gecko/20041001 Firefox/0.10.1" );
+                curl_setopt( $ch, CURLOPT_URL, $origin_url );
+                curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, true );
+                if( strpos($origin_url, 'play.to')){
+                    curl_setopt($ch, CURLOPT_REFERER, "http://fastplay.to/");                
+                 //   curl_setopt($ch, CURLOPT_PROXY, '127.0.0.1:10245');
+                    
+                }
+               curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
+                curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 
-            $result = curl_exec($ch); 
-           // dd($result);           
-            curl_close($ch);
-            if( strpos($result, 'streamable')){
-                $tmp = explode('"url": "', $result);               
-                $tmp = explode('",', $tmp[1]);
-                $video_url = "https:".$tmp[0];    
-                $tmpPoster = explode('"thumbnail_url": "', $result);
-                $tmp = explode('",', $tmpPoster[1]);
-                $poster_url = "https:".$tmp[0];                                      
-            }elseif( strpos($result, 'play.to')){
+                $result = curl_exec($ch); 
+                dd($result);           
+                curl_close($ch);
+            }
+            if( strpos($origin_url, 'play.to')){
                 $crawler = new simple_html_dom();                
-                $crawler->load($result); 
+                $crawler->load_file($origin_url); 
                 $js = $crawler->find('script', 7)->innertext;
                 $unpack = new JavascriptUnpacker;
                 $tmpScript = $unpack->unpack($js);                               
@@ -122,29 +120,58 @@ class HomeController extends Controller
                     $tmp = explode('"', $tmpPoster[1]);   
                     $poster_url = $tmp[0];                 
                 }                                                      
-            }else{                
-                $crawler = new simple_html_dom();                
-                $crawler->load($result); 
-                $js = $crawler->find('script', 4)->innertext;
-                $unpack = new JavascriptUnpacker;
-                $tmpScript = $unpack->unpack($js);                               
-                $tmp = explode('{file:"', $tmpScript);
-                if(isset($tmp[1])){
-                    $tmp = explode('"', $tmp[1]);   
-                    $video_url = $tmp[0];                 
-                }else{
-                    echo ('Video not exists.');die;
-                }
-                $tmpPoster = explode('image:"', $tmpScript);              
-                if(isset($tmpPoster[1])){
-                    $tmp = explode('"', $tmpPoster[1]);   
-                    $poster_url = $tmp[0];                 
-                }                
-            }            
+            }else{
+                if( strpos($result, 'streamable')){
+                    $tmp = explode('"url": "', $result);               
+                    $tmp = explode('",', $tmp[1]);
+                    $video_url = "https:".$tmp[0];    
+                    $tmpPoster = explode('"thumbnail_url": "', $result);
+                    $tmp = explode('",', $tmpPoster[1]);
+                    $poster_url = "https:".$tmp[0];                                      
+                }else{                
+                    $crawler = new simple_html_dom();                
+                    $crawler->load($result); 
+                    $js = $crawler->find('script', 4)->innertext;
+                    $unpack = new JavascriptUnpacker;
+                    $tmpScript = $unpack->unpack($js);                               
+                    $tmp = explode('{file:"', $tmpScript);
+                    if(isset($tmp[1])){
+                        $tmp = explode('"', $tmp[1]);   
+                        $video_url = $tmp[0];                 
+                    }else{
+                        echo ('Video not exists.');die;
+                    }
+                    $tmpPoster = explode('image:"', $tmpScript);              
+                    if(isset($tmpPoster[1])){
+                        $tmp = explode('"', $tmpPoster[1]);   
+                        $poster_url = $tmp[0];                 
+                    }                
+                }  
+            }
+                      
             return view('play', compact('video_url', 'poster_url'));    
         }else{
             dd('Invalid code');
         }
         
     }    
+    // Function to get the client IP address
+public function get_client_ip() {
+    $ipaddress = '';
+    if (getenv('HTTP_CLIENT_IP'))
+        $ipaddress = getenv('HTTP_CLIENT_IP');
+    else if(getenv('HTTP_X_FORWARDED_FOR'))
+        $ipaddress = getenv('HTTP_X_FORWARDED_FOR');
+    else if(getenv('HTTP_X_FORWARDED'))
+        $ipaddress = getenv('HTTP_X_FORWARDED');
+    else if(getenv('HTTP_FORWARDED_FOR'))
+        $ipaddress = getenv('HTTP_FORWARDED_FOR');
+    else if(getenv('HTTP_FORWARDED'))
+       $ipaddress = getenv('HTTP_FORWARDED');
+    else if(getenv('REMOTE_ADDR'))
+        $ipaddress = getenv('REMOTE_ADDR');
+    else
+        $ipaddress = 'UNKNOWN';
+    return $ipaddress;
+}
 }
