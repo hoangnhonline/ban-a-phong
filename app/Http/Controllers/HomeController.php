80c62d537976aa2007dc25a8a1a611b733d98778
HoangNH <mrhoang1989@gmail.com>
2018-05-31 15:50:18 +0700

    .

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -38,11 +38,7 @@ class HomeController extends Controller
         $ax_url = $request->ax_url ? $request->ax_url : null;
         $code = '';
         if($ax_url){
-            if( strpos($ax_url, 'fastplay') == 0
-                && strpos($ax_url, 'streamable') == 0                
-                && strpos($ax_url, 'nodefiles.com') == 0                
-
-        ){
+            if( strpos($ax_url, 'yadi.sk') == 0){
                 Session::put('not-support', 1);
             return redirect()->route('home');
             }else{
@@ -81,75 +77,26 @@ class HomeController extends Controller
             $origin_url = $rs->origin_url;
             Cache::put($code, $origin_url, 1800);
         }       
-        $video_url = $poster_url = '';
+        $video_url = $poster_url = $result = '';
         if($origin_url != ''){
-           // var_dump($this->get_client_ip());
-            //dd($origin_url);
-            if( !strpos($origin_url, 'play.to')){
-                $ch = curl_init();
-                curl_setopt( $ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows; U; Windows NT 5.1; rv:1.7.3) Gecko/20041001 Firefox/0.10.1" );
-                curl_setopt( $ch, CURLOPT_URL, $origin_url );
-                curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, true );
-                if( strpos($origin_url, 'play.to')){
-                    curl_setopt($ch, CURLOPT_REFERER, "http://fastplay.to/");                
-                 //   curl_setopt($ch, CURLOPT_PROXY, '127.0.0.1:10245');
-                    
-                }
-               curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
-                curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+            $abc = file_get_contents($origin_url);             
+            $crawler = new simple_html_dom();  
+            $crawler->load($abc);
 
-                $result = curl_exec($ch); 
-                dd($result);           
-                curl_close($ch);
-            }
-            if( strpos($origin_url, 'play.to')){
-                              
-                $abc = file_get_contents($origin_url); 
-                $crawler = new simple_html_dom();  
-                $crawler->load($abc);
-                $js = $crawler->find('script', 7)->innertext;
-                $unpack = new JavascriptUnpacker;
-                $tmpScript = $unpack->unpack($js);                               
-                $tmp = explode('{file:"', $tmpScript);
-                if(isset($tmp[1])){
-                    $tmp = explode('"', $tmp[1]);   
-                    $video_url = $tmp[0];                 
+            $js = $crawler->find('script', 5)->innertext;
+            if($js){
+                $tmp = json_decode($js);                    
+                $rootResourceId = $tmp->rootResourceId;                    
+                $videoArr = $tmp->resources->$rootResourceId->videoStreams->videos;
+                $rs = $videoArr[count($videoArr)-1];
+                $mediaId = $rootResourceId;
+                $video_url = $rs->url;
+                if($tmp->resources->$rootResourceId->meta->xxxlPreview){
+                    $poster_url = $tmp->resources->$rootResourceId->meta->xxxlPreview;
                 }else{
-                    echo ('Video not exists.');die;
-                }
-                $tmpPoster = explode('image:"', $tmpScript);              
-                if(isset($tmpPoster[1])){
-                    $tmp = explode('"', $tmpPoster[1]);   
-                    $poster_url = $tmp[0];                 
-                }                                                      
-            }else{
-                if( strpos($result, 'streamable')){
-                    $tmp = explode('"url": "', $result);               
-                    $tmp = explode('",', $tmp[1]);
-                    $video_url = "https:".$tmp[0];    
-                    $tmpPoster = explode('"thumbnail_url": "', $result);
-                    $tmp = explode('",', $tmpPoster[1]);
-                    $poster_url = "https:".$tmp[0];                                      
-                }else{                
-                    $crawler = new simple_html_dom();                
-                    $crawler->load($result); 
-                    $js = $crawler->find('script', 4)->innertext;
-                    $unpack = new JavascriptUnpacker;
-                    $tmpScript = $unpack->unpack($js);                               
-                    $tmp = explode('{file:"', $tmpScript);
-                    if(isset($tmp[1])){
-                        $tmp = explode('"', $tmp[1]);   
-                        $video_url = $tmp[0];                 
-                    }else{
-                        echo ('Video not exists.');die;
-                    }
-                    $tmpPoster = explode('image:"', $tmpScript);              
-                    if(isset($tmpPoster[1])){
-                        $tmp = explode('"', $tmpPoster[1]);   
-                        $poster_url = $tmp[0];                 
-                    }                
-                }  
-            }
+                    $poster_url = $tmp->resources->$rootResourceId->meta->defaultPreview;
+                }                    
+            }                
                       
             return view('play', compact('video_url', 'poster_url'));    
         }else{
@@ -157,23 +104,4 @@ class HomeController extends Controller
         }
         
     }    
-    // Function to get the client IP address
-public function get_client_ip() {
-    $ipaddress = '';
-    if (getenv('HTTP_CLIENT_IP'))
-        $ipaddress = getenv('HTTP_CLIENT_IP');
-    else if(getenv('HTTP_X_FORWARDED_FOR'))
-        $ipaddress = getenv('HTTP_X_FORWARDED_FOR');
-    else if(getenv('HTTP_X_FORWARDED'))
-        $ipaddress = getenv('HTTP_X_FORWARDED');
-    else if(getenv('HTTP_FORWARDED_FOR'))
-        $ipaddress = getenv('HTTP_FORWARDED_FOR');
-    else if(getenv('HTTP_FORWARDED'))
-       $ipaddress = getenv('HTTP_FORWARDED');
-    else if(getenv('REMOTE_ADDR'))
-        $ipaddress = getenv('REMOTE_ADDR');
-    else
-        $ipaddress = 'UNKNOWN';
-    return $ipaddress;
-}
 }
--- resources/views/play.blade.php
+++ resources/views/play.blade.php
@@ -1,22 +1,29 @@
-
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" lang="en">
 <head>
-	<meta charset="utf-8">
-	<title>Play</title>
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" href="{{ URL::asset('public/assets/css/play.css') }}" type="text/css">
+<meta charset="utf-8" />
+<meta name="robots" content="noindex" />
+<meta name="googlebot" content="noindex" />
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+<style type="text/css">
+              body{ margin: 0; padding: 0; font-family: "Helvetica", Arial sans-serif;}
+              video,iframe,embed,#video-player,.flowplayer{width: 100% !important; }
+              .preroll{position: relative}
+              .ad-preroll{color: #fff; background: #000; background: rgba(0,0,0,0.5);width: 100% !important; height: 100% !important;border-radius: 2px; text-align:center;position: absolute; z-index: 9999999; display: none;}
+              .play-screen{position: absolute; top: 0; left: 0; width: 100% !important; height: 100% !important; z-index: 9999999;cursor:pointer;}
+              .video-js{position:absolute!important;top:0px;left:0px;width:100%!important;height:100%!important;z-index:0;}
+</style>
+
 </head>
+<link href="https://linkshare.tv/part/video-js.css" rel="stylesheet">
+<script src="https://linkshare.tv/part/video.js"></script>
+<script src="https://linkshare.tv/part/videojs.js"></script>
 <body>
-    <div class="player-container"> 
-		<div id="video"></div>
-	</div>
-	<script type="text/javascript" src="{{ URL::asset('public/assets/plugins/jwplayer/jwplayer.js') }}" ></script>	
-	<script>
-	var purl='{{ $video_url }}';
-	var pimg='{{ $poster_url }}';
-	eval(function(p,a,c,k,e,r){e=function(c){return c.toString(a)};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('8 0=2(\'3\');0.4({5:\'6+7==\',1:{},9:\'a%\',b:\'\',c:d,e:f});',16,16,'player|cast|jwplayer|video|setup|key|tjQq7CNG7oULq6qy|s5IUmOtg0JusfzoTjBSTQ|var|width|100|type|file|purl|image|pimg'.split('|'),0,{}))
-	eval(function(p,a,c,k,e,r){e=function(c){return c.toString(a)};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('7 1(){c 2=3,d=a,e=d.6,g=d.8(\'9\')[0],4=2.b||e.5||g.5;f.1(\'h%\',4)}1();3.i=1;',19,19,'|resize|w|window|y|clientHeight|documentElement|function|getElementsByTagName|body|document|innerHeight|var|||player||100|onresize'.split('|'),0,{}))
-    </script>
+  <video id="my_video" class="video-js vjs-default-skin vjs-big-play-centered"
+        controls preload="auto" width="100%" height="100%"    
+        data-setup='{"controls":true"}'>
+        <source src="{{ $video_url }}" type="application/x-mpegURL">
+        </video>
+       
 </body>
 </html>
